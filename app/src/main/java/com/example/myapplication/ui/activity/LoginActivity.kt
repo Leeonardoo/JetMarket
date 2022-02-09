fc9423f1c9d528a095ac98e6914774964d0f6362
Leeonardoo <leeonardo.deo@gmail.com>
2022-02-08 21:15:46 -0300

    More edge-to-edge magic Added Inset-aware TopAppBar components and a "ActionMenu"

--- app/src/main/java/com/example/myapplication/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/LoginActivity.kt
@@ -3,34 +3,68 @@ package com.example.myapplication.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.rememberSplineBasedDecay
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import com.example.myapplication.ui.components.InsetLargeTopAppBar
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.accompanist.insets.ProvideWindowInsets
 
+@OptIn(ExperimentalMaterial3Api::class)
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
+            val decayAnimationSpec = rememberSplineBasedDecay<Float>()
+            val scrollBehavior = remember(decayAnimationSpec) {
+                TopAppBarDefaults.exitUntilCollapsedScrollBehavior(decayAnimationSpec)
+            }
+            val scrollState = rememberScrollState()
 
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface,
-                    tonalElevation = 1.dp
-                ) {
-                    Greeting2("Android")
+                ProvideWindowInsets {
+                    Scaffold(
+                        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                        topBar = {
+                            InsetLargeTopAppBar(
+                                title = { Text("Title") },
+                                scrollBehavior = scrollBehavior
+                            )
+                        },
+                        content = { paddingValues ->
+                            // A surface container using the 'background' color from the theme
+                            Surface(
+                                modifier = Modifier
+                                    .padding(paddingValues)
+                                    .fillMaxSize(),
+                                color = MaterialTheme.colorScheme.surface,
+                                tonalElevation = 1.dp
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(paddingValues)
+                                        .fillMaxSize()
+                                        .verticalScroll(scrollState)
+                                ) {
+                                    repeat(74) {
+                                        Greeting2("Android")
+                                    }
+                                }
+                            }
+                        })
                 }
             }
         }
---
+++ app/src/main/java/com/example/myapplication/ui/components/ActionMenu.kt
@@ -0,0 +1,181 @@
+package com.example.myapplication.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.R
+
+/**
+ * A custom implementation of a toolbar "menu" similar to how it is done using XML and Views, while
+ * we still don't have that functionality officially.
+ *
+ * The code can be found at a gist from MachFour
+ * @see <a href="https://gist.github.com/MachFour/369ebb56a66e2f583ebfb988dda2decf">Original Gist</a>
+ */
+
+// Kind of equivalent to a menu XML entry, except for the onClick lambda
+data class ActionItemSpec(
+    val modifier: Modifier = Modifier,
+    val name: String,
+    val painter: Painter,
+    val visibility: ActionItemMode = ActionItemMode.IF_ROOM,
+    val onClick: () -> Unit
+) {
+    companion object {
+        @Composable
+        fun fromImageVector(
+            modifier: Modifier = Modifier,
+            name: String,
+            imageVector: ImageVector,
+            visibility: ActionItemMode = ActionItemMode.IF_ROOM,
+            onClick: () -> Unit
+        ): ActionItemSpec = ActionItemSpec(
+            modifier,
+            name,
+            rememberVectorPainter(image = imageVector),
+            visibility,
+            onClick
+        )
+    }
+}
+
+// Whether to show the action item as an icon or not (or if room)
+enum class ActionItemMode {
+    ALWAYS_SHOW, IF_ROOM, NEVER_SHOW
+}
+
+@Preview
+@Composable
+fun PreviewActionMenu() {
+    val items = listOf(
+        ActionItemSpec.fromImageVector(
+            name = "Call",
+            imageVector = Icons.Default.Call,
+            visibility = ActionItemMode.ALWAYS_SHOW
+        ) {},
+        ActionItemSpec.fromImageVector(
+            name = "Send",
+            imageVector = Icons.Default.Send,
+            visibility = ActionItemMode.IF_ROOM
+        ) {},
+        ActionItemSpec.fromImageVector(
+            name = "Email",
+            imageVector = Icons.Default.Email,
+            visibility = ActionItemMode.IF_ROOM
+        ) {},
+        ActionItemSpec.fromImageVector(
+            name = "Delete",
+            imageVector = Icons.Default.Delete,
+            visibility = ActionItemMode.IF_ROOM
+        ) {},
+    )
+    TopAppBar(
+        title = { Text("App bar") },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(Icons.Default.Menu, "Menu")
+            }
+        },
+        actions = {
+            ActionMenu(items, defaultIconSpace = 3)
+        }
+    )
+}
+
+
+@Composable
+fun ActionMenu(
+    items: List<ActionItemSpec>,
+    defaultIconSpace: Int = 3, // includes overflow menu
+    menuExpanded: MutableState<Boolean> = remember { mutableStateOf(false) }
+) {
+    // decide how many ifRoom icons to show as actions
+    val (actionItems, overflowItems) = remember(items, defaultIconSpace) {
+        separateIntoActionAndOverflow(items, defaultIconSpace)
+    }
+
+    Row {
+        for (item in actionItems) {
+            IconButton(onClick = item.onClick) {
+                Icon(
+                    modifier = item.modifier,
+                    painter = item.painter,
+                    contentDescription = item.name
+                )
+            }
+        }
+        if (overflowItems.isNotEmpty()) {
+            IconButton(onClick = { menuExpanded.value = true }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = stringResource(id = R.string.more),
+                )
+            }
+            DropdownMenu(
+                expanded = menuExpanded.value,
+                onDismissRequest = { menuExpanded.value = false }
+            ) {
+                for (item in overflowItems) {
+                    DropdownMenuItem(onClick = item.onClick) {
+                        //Icon(item.icon, item.name) just have text in the overflow menu
+                        Text(item.name)
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun separateIntoActionAndOverflow(
+    items: List<ActionItemSpec>,
+    defaultIconSpace: Int
+): Pair<List<ActionItemSpec>, List<ActionItemSpec>> {
+    var (alwaysCount, neverCount, ifRoomCount) = Triple(0, 0, 0)
+    for (item in items) {
+        when (item.visibility) {
+            ActionItemMode.ALWAYS_SHOW -> alwaysCount++
+            ActionItemMode.NEVER_SHOW -> neverCount++
+            ActionItemMode.IF_ROOM -> ifRoomCount++
+        }
+    }
+
+    val needsOverflow = alwaysCount + ifRoomCount > defaultIconSpace || neverCount > 0
+    val actionIconSpace = defaultIconSpace - (if (needsOverflow) 1 else 0)
+
+    val actionItems = ArrayList<ActionItemSpec>()
+    val overflowItems = ArrayList<ActionItemSpec>()
+
+    var ifRoomsToDisplay = actionIconSpace - alwaysCount
+    for (item in items) {
+        when (item.visibility) {
+            ActionItemMode.ALWAYS_SHOW -> {
+                actionItems.add(item)
+            }
+            ActionItemMode.NEVER_SHOW -> {
+                overflowItems.add(item)
+            }
+            ActionItemMode.IF_ROOM -> {
+                if (ifRoomsToDisplay > 0) {
+                    actionItems.add(item)
+                    ifRoomsToDisplay--
+                } else {
+                    overflowItems.add(item)
+                }
+
+            }
+        }
+    }
+    return Pair(actionItems, overflowItems)
+}
