50ed3c96f218fd102198000df0d73b757243920d
Leeonardoo <leeonardo.deo@gmail.com>
2022-02-09 22:55:47 -0300

    Trying to adapt a Material2 theme with Material3 colors Added custom Material3 colorScheme for testing Added M3TextField.kt which applies (some of) the current Material3 colors

--- app/src/main/java/com/example/myapplication/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/LoginActivity.kt
@@ -9,15 +9,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import com.example.myapplication.ui.components.M3TextField
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.insets.systemBarsPadding
@@ -47,7 +47,8 @@ fun LoginContent() {
             modifier = Modifier
                 .verticalScroll(scrollState)
                 .systemBarsPadding(),
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = "Title",
@@ -55,6 +56,14 @@ fun LoginContent() {
                 style = MaterialTheme.typography.headlineMedium,
                 textAlign = TextAlign.Center
             )
+
+            M3TextField(value = "", label = { androidx.compose.material.Text(text = "Hint")}, onValueChange = {})
+
+            FilledTonalButton(onClick = { /*TODO*/ }) {
+                Text(text = "Test")
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -1,43 +1,16 @@
 package com.example.myapplication.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun MyApplicationTheme(
@@ -82,7 +55,106 @@ fun MyApplicationTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
+        typography = Typography
+    ) {
+        // TODO: Remove M2 MaterialTheme when using only M3 components
+        androidx.compose.material.MaterialTheme(
+            colors = if (darkTheme) M2DarkColors() else M2LightColors(),
+            content = content
+        )
+    }
+
+    /*MaterialTheme(
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
+    )*/
+}
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFF984816),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFFFDBC9),
+    onPrimaryContainer = Color(0xFF341000),
+    inversePrimary = Color(0xFFFFB68F),
+    secondary = Color(0xFF765849),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFFFFDBC9),
+    onSecondaryContainer = Color(0xFF2B160B),
+    tertiary = Color(0xFF656032),
+    onTertiary = Color(0xFFFFFFFF),
+    tertiaryContainer = Color(0xFFEBE4AA),
+    onTertiaryContainer = Color(0xFF1F1C00),
+    background = Color(0xFFFCFCFC),
+    onBackground = Color(0xFF201A17),
+    surface = Color(0xFFFCFCFC),
+    onSurface = Color(0xFF201A17),
+    surfaceVariant = Color(0xFFF4DED5),
+    onSurfaceVariant = Color(0xFF53443D),
+    inverseSurface = Color(0xFF362F2C),
+    inverseOnSurface = Color(0xFFFBEEE9),
+    error = Color(0xFFBA1B1B),
+    onError = Color(0xFFFFFFFF),
+    errorContainer = Color(0xFFFFDAD4),
+    onErrorContainer = Color(0xFF410001),
+    outline = Color(0xFF85736B),
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFFFFB68F),
+    onPrimary = Color(0xFF562000),
+    primaryContainer = Color(0xFF793100),
+    onPrimaryContainer = Color(0xFFFFDBC9),
+    inversePrimary = Color(0xFF984816),
+    secondary = Color(0xFFE6BEAC),
+    onSecondary = Color(0xFF432B1E),
+    secondaryContainer = Color(0xFF5C4032),
+    onSecondaryContainer = Color(0xFFFFDBC9),
+    tertiary = Color(0xFFCFC890),
+    onTertiary = Color(0xFF353107),
+    tertiaryContainer = Color(0xFF4C481C),
+    onTertiaryContainer = Color(0xFFEBE4AA),
+    background = Color(0xFF201A17),
+    onBackground = Color(0xFFEDE0DB),
+    surface = Color(0xFF201A17),
+    onSurface = Color(0xFFEDE0DB),
+    surfaceVariant = Color(0xFF53443D),
+    onSurfaceVariant = Color(0xFFD7C2B9),
+    inverseSurface = Color(0xFFEDE0DB),
+    inverseOnSurface = Color(0xFF362F2C),
+    error = Color(0xFFFFB4A9),
+    onError = Color(0xFF680003),
+    errorContainer = Color(0xFF930006),
+    onErrorContainer = Color(0xFFFFDAD4),
+    outline = Color(0xFFA08D85),
+)
+
+private val M2LightColors = @Composable {
+    lightColors(
+        primary = MaterialTheme.colorScheme.primary,
+        primaryVariant =MaterialTheme.colorScheme.primaryContainer,
+        secondary = MaterialTheme.colorScheme.secondary,
+        secondaryVariant = MaterialTheme.colorScheme.secondaryContainer,
+        background = MaterialTheme.colorScheme.background,
+        surface = MaterialTheme.colorScheme.surface,
+        onPrimary = MaterialTheme.colorScheme.onPrimary,
+        onSecondary = MaterialTheme.colorScheme.onSecondary,
+        onBackground = MaterialTheme.colorScheme.onBackground,
+        onSurface = MaterialTheme.colorScheme.onSurface
+    )
+}
+
+private val M2DarkColors = @Composable {
+    darkColors(
+        primary = MaterialTheme.colorScheme.primary,
+        primaryVariant =MaterialTheme.colorScheme.primaryContainer,
+        secondary = MaterialTheme.colorScheme.secondary,
+        secondaryVariant = MaterialTheme.colorScheme.secondaryContainer,
+        background = MaterialTheme.colorScheme.background,
+        surface = MaterialTheme.colorScheme.surface,
+        onPrimary = MaterialTheme.colorScheme.onPrimary,
+        onSecondary = MaterialTheme.colorScheme.onSecondary,
+        onBackground = MaterialTheme.colorScheme.onBackground,
+        onSurface = MaterialTheme.colorScheme.onSurface
     )
 }
